8d37b3f53b0adb773a8582f614c272723a530c55
Chaeyoung Oh <86927020+chaeraemong@users.noreply.github.com>
2025-06-03 03:26:12 +0900

    Update gunicorn.conf.py

--- LLAVA/gunicorn.conf.py
+++ LLAVA/gunicorn.conf.py
@@ -1,8 +1,8 @@
 import os
-NUM_DEVICES = 2
+NUM_DEVICES = 1
 USED_DEVICES = set()
 
-os.environ["LLAVA_PARAMS_PATH"] = "../llava-weights"
+os.environ["LLAVA_PARAMS_PATH"] = "liuhaotian/llava-v1.6-vicuna-7b"    #llava-hf/llava-1.5-7b-hf"    #liuhaotian/llava-v1.6-mistral-7b"    #../llava-weights"
 
 def pre_fork(server, worker):
     # runs on server
@@ -20,7 +20,7 @@ def child_exit(server, worker):
     USED_DEVICES.remove(worker.device_id)
 
 # Gunicorn Configuration
-bind = "127.0.0.1:8085"
+bind = "0.0.0.0:8000"
 workers = NUM_DEVICES
 worker_class = "sync"
 timeout = 120
